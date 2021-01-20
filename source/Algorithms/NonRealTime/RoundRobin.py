0ba144ee155adac717961e9c89fb8f2e76706196
joeric1028 <joeric1028@icloud.com>
2021-01-20 21:52:37 +0800

    - Fixed Class definition of both Non Real Time Algorithm (Round Robin and Shortest Job First). - Renamed class variables/functions to its standard Python coding.

--- source/Algorithms/NonRealTime/RoundRobin.py
+++ source/Algorithms/NonRealTime/RoundRobin.py
@@ -3,48 +3,46 @@
 # DEVELOPED BY: ELIZABELLE SERDIÑA
 # SUPERVISED BY: GEORGE CHARALAMBOUS
 # (C)2020
-
-import matplotlib.pyplot as plt
-import random
 import time
 
 
 class RoundRobin:
-    averageWaitingTime = 0
-    totalProc = 0
-    waitList = []
-    start = 0
+    def __init__(self):
+        self.__averageWaitingTime = 0
+        self.__averageTurnAroundTime = 0
+        self.__totalProc = 0
+        self.__waitList = []
+        self.__start = 0
+        self.__execProcess = 0
 
-    def createProcess(self, processData,
-                      quantum):  # this function will create and gather the data of each of the processes
-        # quantum = int(input("Please Enter Time Quantum: "))
-        global start
-        start = time.time()
-        RoundRobin.executeProcesses(self, processData, quantum)
+    def createprocess(self, processdata, quantum):
+        # this function will create and gather the data of each of the processes
+        self.__start = time.time()
+        self.__executeprocesses(processdata, quantum)
 
-    def executeProcesses(self, processData, quantum):  # this will execute the round robin algorithm
+    def __executeprocesses(self, processdata, quantum):  # this will execute the round robin algorithm
         startTime = []
         exitTime = []
         execProcess = []
         readyQueue = []
         stime = 0
-        processData.sort(key=lambda x: x[1])  # this will sort the processes by their arrival time
+        processdata.sort(key=lambda x: x[1])  # this will sort the processes by their arrival time
 
         while 1:
             normalQueue = []  # this is where we store all the processes that haven't arrived yet
             temp = []
-            for i in range(len(processData)):
-                if processData[i][1] <= stime and processData[i][3] == 0:
+            for i in range(len(processdata)):
+                if processdata[i][1] <= stime and processdata[i][3] == 0:
                     present = 0
                     if len(readyQueue) != 0:
                         for k in range(len(readyQueue)):
-                            if processData[i][0] == readyQueue[k][0]:
+                            if processdata[i][0] == readyQueue[k][0]:
                                 present = 1
 
                     # the above if checks that the next process is not a part of the ready queue
 
                     if present == 0:
-                        temp.extend([processData[i][0], processData[i][1], processData[i][2], processData[i][4]])
+                        temp.extend([processdata[i][0], processdata[i][1], processdata[i][2], processdata[i][4]])
                         readyQueue.append(temp)
                         temp = []
 
@@ -55,10 +53,11 @@ class RoundRobin:
                             if readyQueue[k][0] == execProcess[len(execProcess) - 1]:
                                 readyQueue.insert((len(readyQueue) - 1), readyQueue.pop(k))
 
-                    # the above if makes sure that the recently executed process is appended at the end of the ready queue
+                    # the above if makes sure that the recently executed process is appended at the end of the ready
+                    # queue
 
-                elif processData[i][3] == 0:
-                    temp.extend([processData[i][0], processData[i][1], processData[i][2], processData[i][4]])
+                elif processdata[i][3] == 0:
+                    temp.extend([processdata[i][0], processdata[i][1], processdata[i][2], processdata[i][4]])
                     normalQueue.append(temp)
                     temp = []
             if len(readyQueue) == 0 and len(normalQueue) == 0:
@@ -72,10 +71,12 @@ class RoundRobin:
                     etime = stime
                     exitTime.append(etime)
                     execProcess.append(readyQueue[0][0])
-                    for j in range(len(processData)):
-                        if processData[j][0] == readyQueue[0][0]:
+                    jk = 0
+                    for j in range(len(processdata)):
+                        if processdata[j][0] == readyQueue[0][0]:
+                            jk = j
                             break
-                    processData[j][2] = processData[j][2] - quantum
+                    processdata[jk][2] = processdata[jk][2] - quantum
                     readyQueue.pop(0)
                 elif readyQueue[0][2] <= quantum:
                     # If a process has a remaining burst time less than or equal to time quantum,
@@ -85,12 +86,14 @@ class RoundRobin:
                     etime = stime
                     exitTime.append(etime)
                     execProcess.append(readyQueue[0][0])
-                    for j in range(len(processData)):
-                        if processData[j][0] == readyQueue[0][0]:
+                    jk = 0
+                    for j in range(len(processdata)):
+                        if processdata[j][0] == readyQueue[0][0]:
+                            jk = j
                             break
-                    processData[j][2] = 0
-                    processData[j][3] = 1
-                    processData[j].append(etime)
+                    processdata[jk][2] = 0
+                    processdata[jk][3] = 1
+                    processdata[jk].append(etime)
                     readyQueue.pop(0)
             elif len(readyQueue) == 0:
                 if stime < normalQueue[0][1]:
@@ -103,10 +106,12 @@ class RoundRobin:
                     etime = stime
                     exitTime.append(etime)
                     execProcess.append(normalQueue[0][0])
-                    for j in range(len(processData)):
-                        if processData[j][0] == normalQueue[0][0]:
+                    jk = 0
+                    for j in range(len(processdata)):
+                        if processdata[j][0] == normalQueue[0][0]:
+                            jk = j
                             break
-                    processData[j][2] = processData[j][2] - quantum
+                    processdata[jk][2] = processdata[jk][2] - quantum
                 elif normalQueue[0][2] <= quantum:
                     # If a process has a remaining burst time less than or equal to time quantum,
                     # it will complete its execution
@@ -115,73 +120,68 @@ class RoundRobin:
                     etime = stime
                     exitTime.append(etime)
                     execProcess.append(normalQueue[0][0])
-                    for j in range(len(processData)):
-                        if processData[j][0] == normalQueue[0][0]:
+                    jk = 0
+                    for j in range(len(processdata)):
+                        if processdata[j][0] == normalQueue[0][0]:
+                            jk = j
                             break
-                    processData[j][2] = 0
-                    processData[j][3] = 1
-                    processData[j].append(etime)
+                    processdata[jk][2] = 0
+                    processdata[jk][3] = 1
+                    processdata[jk].append(etime)
 
-        turnAroundTime = RoundRobin.calculateTurnaroundTime(self, processData)
-        waitingTime = RoundRobin.calculateWaitingTime(self, processData)
-        global averageWaitingTime
-        averageWaitingTime = waitingTime
-        RoundRobin.printData(self, processData, turnAroundTime, waitingTime, execProcess)
+        self.__execProcess = execProcess
+        self.__calculateturnaroundtime(processdata)
+        self.__calculatewaitingtime(processdata)
+        self.printdata(processdata)
 
-    def calculateTurnaroundTime(self, processData):
+    def __calculateturnaroundtime(self, processdata):
         totalTurnAroundTime = 0
-        for i in range(len(processData)):
-            turnAroundTime = processData[i][5] - processData[i][1]
+        for i in range(len(processdata)):
+            turnAroundTime = processdata[i][5] - processdata[i][1]
 
             # turnAroundTime = completionTime - arrivalTime
 
             totalTurnAroundTime = totalTurnAroundTime + turnAroundTime
-            processData[i].append(turnAroundTime)
-        avgTurnAroundTime = totalTurnAroundTime / len(processData)
-
+            processdata[i].append(turnAroundTime)
+        self.__averageTurnAroundTime = totalTurnAroundTime / len(processdata)
         # avgTurnAroundTime = totalTurnAroundTime / numOfProcesses
-        return avgTurnAroundTime
 
-    def calculateWaitingTime(self, processData):
+    def __calculatewaitingtime(self, processdata):
         totalWaitingTime = 0
-        for i in range(len(processData)):
-            waitingTime = processData[i][6] - processData[i][4]
+        for i in range(len(processdata)):
+            waitingTime = processdata[i][6] - processdata[i][4]
 
             # waitingTime = turnAroundTime - burstTime
 
             totalWaitingTime = totalWaitingTime + waitingTime
-            processData[i].append(waitingTime)
-        avgWaitingTime = totalWaitingTime / len(processData)
-
+            processdata[i].append(waitingTime)
+        self.__averageWaitingTime = totalWaitingTime / len(processdata)
         # avgWaitingTime = totalWaitingTime / numOfProcesses
-        return avgWaitingTime
 
-    def printData(self, processData, avgTurnAroundTime, avgWaitingTime, execProcess):
-        processData.sort(key=lambda x: x[0])  # this will sort the processes by their IDs
-        print(
-            "Process ID  Arrival Time  Rem Burst Time   Completed  Original Burst Time  Completion Time  Turnaround Time  Waiting Time")
-        for i in range(len(processData)):
-            for j in range(len(processData[i])):
-                print(processData[i][j], end="				")
+    def printdata(self, processdata):
+        waitingTimeList = []
+        processdata.sort(key=lambda y: y[0])  # this will sort the processes by their IDs
+        print("Round Robin Algorithm Simulation Results\n\n"
+              "Process ID  Arrival Time  Rem Burst Time   Completed  Original Burst Time  Completion Time  Turnaround "
+              "Time  Waiting Time")
+        for i in range(len(processdata)):
+            for j in range(len(processdata[i])):
+                print(processdata[i][j], end="				")
             print()
 
-        print(f'Average Turnaround Time: {avgTurnAroundTime}ms')
+        print(f'Average Turnaround Time: {self.__averageTurnAroundTime}')
+        print(f'Average Waiting Time: {self.__averageWaitingTime}')
 
-        print(f'Average Waiting Time: {avgWaitingTime}ms')
+        # print(f'Sequence of Processes: {self.execProcess}')
 
-        print(f'Sequence of Processes: {execProcess}')
-
-        for x in range(len(processData)):
+        for x in range(len(processdata)):
             waitingTimeList = []
             processList = []
-            waitingTimeList.append(processData[x][7])
-            processList.append(processData[x][0])
-        waitList = waitingTimeList
+            waitingTimeList.append(processdata[x][7])
+            processList.append(processdata[x][0])
+        self.__waitList = waitingTimeList
         end = time.time()
-        print(f"Round-Robin Execution Time = {end - start}")
+        print(f"Round-Robin Execution Time = {end - self.__start} seconds\n\n")
 
-    def getAvgWaitTime(self):
-        return averageWaitingTime
-
-    def getNumOfProcesses(self):
-        return totalProc
+    def getavgwaittime(self):
+        return self.__averageWaitingTime
