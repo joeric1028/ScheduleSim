2ec5b3b2cd4aaf588d42788b26316eaa7354078b
joeric1028 <joeric1028@icloud.com>
2021-01-20 21:52:37 +0800

    Non Real Algorithm Update
    - Fixed Class definition of both Non Real Time Algorithm (Round Robin and Shortest Job First).
    - Renamed class variables/functions to its standard

--- source/Algorithms/NonRealTime/ShortestJobFirst.py
+++ source/Algorithms/NonRealTime/ShortestJobFirst.py
@@ -3,39 +3,37 @@
 # DEVELOPED BY: ELIZABELLE SERDIÑA
 # SUPERVISED BY: GEORGE CHARALAMBOUS
 # (C)2020
-
-import random
 import time
 
 
 class ShortestJobFirst:
-    averageWaitingTime = 0
-    totalProc = 0
-    start = 0
+    def __init__(self):
+        self.averageWaitingTime = 0
+        self.averageTurnAroundTime = 0
+        self.__start = 0
 
-    def createProcess(self, processData):  # this function will create and gather the data of each of the processes
-        global start
-        start = time.time()
-        ShortestJobFirst.executeProcess(self, processData)
+    def createprocess(self, processdata):  # this function will create and gather the data of each of the processes
+        self.__start = time.time()
+        self.__executeprocess(processdata)
 
-    def executeProcess(self, processData):  # this will execute the shortest job first algorithm
+    def __executeprocess(self, processdata):  # this will execute the shortest job first algorithm
         startTime = []
         exitTime = []
         sTime = 0
-        processData.sort(key=lambda x: x[1])  # this will sort the processes by their arrival time
+        processdata.sort(key=lambda x: x[1])  # this will sort the processes by their arrival time
 
-        for i in range(len(processData)):
+        for i in range(len(processdata)):
             readyQueue = []
             temp = []
             normalQueue = []
 
-            for j in range(len(processData)):
-                if (processData[j][1] <= sTime) and (processData[j][3] == 0):
-                    temp.extend([processData[j][0], processData[j][1], processData[j][2]])
+            for j in range(len(processdata)):
+                if (processdata[j][1] <= sTime) and (processdata[j][3] == 0):
+                    temp.extend([processdata[j][0], processdata[j][1], processdata[j][2]])
                     readyQueue.append(temp)
                     temp = []
-                elif processData[j][3] == 0:
-                    temp.extend([processData[j][0], processData[j][1], processData[j][2]])
+                elif processdata[j][3] == 0:
+                    temp.extend([processdata[j][0], processdata[j][1], processdata[j][2]])
                     normalQueue.append(temp)
                     temp = []
 
@@ -46,11 +44,13 @@ class ShortestJobFirst:
                 sTime = sTime + readyQueue[0][2]
                 eTime = sTime
                 exitTime.append(eTime)
-                for k in range(len(processData)):
-                    if processData[k][0] == readyQueue[0][0]:
+                kj = 0
+                for k in range(len(processdata)):
+                    kj = k
+                    if processdata[k][0] == readyQueue[0][0]:
                         break
-                processData[k][3] = 1
-                processData[k].append(eTime)
+                processdata[kj][3] = 1
+                processdata[kj].append(eTime)
 
             elif len(readyQueue) == 0:
                 if sTime < normalQueue[0][1]:
@@ -59,63 +59,66 @@ class ShortestJobFirst:
                 sTime = sTime + normalQueue[0][2]
                 eTime = sTime
                 exitTime.append(eTime)
-                for k in range(len(processData)):
-                    if processData[k][0] == normalQueue[0][0]:
+                kj = 0
+                for k in range(len(processdata)):
+                    kj = k
+                    if processdata[k][0] == normalQueue[0][0]:
                         break
-                processData[k][3] = 1
-                processData[k].append(eTime)
+                processdata[kj][3] = 1
+                processdata[kj].append(eTime)
 
-        totalTime = ShortestJobFirst.calculateTurnaroundTime(self, processData)
-        waitingTime = ShortestJobFirst.calculateWaitingTime(self, processData)
-        global averageWaitingTime
-        averageWaitingTime = waitingTime
-        ShortestJobFirst.printData(self, processData, totalTime, waitingTime)
+        self.__calculateturnaroundtime(processdata)
+        self.__calculatewaitingtime(processdata)
+        self.printdata(processdata)
 
-    def calculateTurnaroundTime(self, processData):
+    def __calculateturnaroundtime(self, processdata):
         totalTurnAroundTime = 0
-        for i in range(len(processData)):
-            turnAroundTime = processData[i][4] - processData[i][1]
+        for i in range(len(processdata)):
+            if len(processdata[i]) < 4:  # TODO: Could not calculate unfinished process Data. partial Fixed.
+                turnAroundTime = 0
+            else:
+                turnAroundTime = processdata[i][4] - processdata[i][1]
 
             # turnAroundTime = completionTime - arrivalTime
 
             totalTurnAroundTime = totalTurnAroundTime + turnAroundTime
-            processData[i].append(turnAroundTime)
-        avgTurnAroundTime = totalTurnAroundTime / len(processData)
+            processdata[i].append(turnAroundTime)
+        self.averageTurnAroundTime = totalTurnAroundTime / len(processdata)
         # averageTurnAroundTime = totalTurnAroundTime / numOfProcesses
 
-        return avgTurnAroundTime
-
-    def calculateWaitingTime(self, processData):
+    def __calculatewaitingtime(self, processdata):
         totalWaitingTime = 0
-        for i in range(len(processData)):
-            waitingTime = processData[i][5] - processData[i][2]
+        for i in range(len(processdata)):
+            if len(processdata[i]) < 6:  # TODO: Same problem occured and partially fixed.
+                waitingTime = 0
+            else:
+                waitingTime = processdata[i][5] - processdata[i][2]
 
             # waitingTime = turnAroundTime - burstTime
 
             totalWaitingTime = totalWaitingTime + waitingTime
-            processData[i].append(waitingTime)
-        avgWaitingTime = totalWaitingTime / len(processData)
+            processdata[i].append(waitingTime)
+        self.averageWaitingTime = totalWaitingTime / len(processdata)
         # averageWaitingTime = totalWaitingTime / noOfProcesses
 
-        return avgWaitingTime
+    def printdata(self, processdata):
+        processdata.sort(key=lambda x: x[0])  # this will sort the processes by their IDs
 
-    def printData(self, processData, avgTurnAroundTime, avgWaitingTime):
-        processData.sort(key=lambda x: x[0])  # this will sort the processes by their IDs
+        print("Shortest Job First Algorithm Simulation Result\n\n"
+              "processID  arrivalTime  burstTime      Completed  Completion_Time  turnAroundTime  WaitTime")
 
-        print("processID  arrivalTime  burstTime      Completed  Completion_Time  turnAroundTime  WaitTime")
-
-        for i in range(len(processData)):
-            for j in range(len(processData[i])):
-                print(processData[i][j], end="				")
+        for i in range(len(processdata)):
+            for j in range(len(processdata[i])):
+                print(processdata[i][j], end="				")
             print()
 
-        print(f'Average Turnaround Time: {avgTurnAroundTime}')
-        print(f'Average Waiting Time: {avgWaitingTime}')
+        print(f'Average Turnaround Time: {self.averageTurnAroundTime}')
+        print(f'Average Waiting Time: {self.averageWaitingTime}')
         end = time.time()
-        print(f"Shortest-Job-First Execution Time = {end - start}")
+        print(f"Shortest-Job-First Execution Time = {end - self.__start} seconds\n\n")
 
-    def getAvgWaitTime(self):
-        return averageWaitingTime
+    def getavgwaittime(self):
+        return self.averageWaitingTime
 
-    def getNumOfProcesses(self):
-        return totalProc
+    def getavgturnaroundtime(self):
+        return self.averageTurnAroundTime
